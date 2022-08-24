32eb5d07fb689991977af56d47c35267d30009b8
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-24 15:48:00 +0300

    task_2 logic v2

--- hw.02_220822/task_2/Program.cs
+++ hw.02_220822/task_2/Program.cs
@@ -11,24 +11,31 @@ int random_digit = new Random().Next(1, 65536); //65536
 Console.WriteLine("Случайно сгенерированное число: " + random_digit);
 
 string digit_len = Convert.ToString(random_digit);
-Console.WriteLine(digit_len.Length);
+// Console.WriteLine(digit_len.Length);
+
+
+Console.Write("ОТВЕТ: ");
+int result = 0;
+// string print_result_345 = ("У числа " + random_digit + " третья цифра является " + result + ".\n\n");
+
 
 if (digit_len.Length <= 2)
 {
     Console.WriteLine("У числа " + random_digit + " третьей цифры нет.");
 }
-else if (digit_len.Length == 3)
+else
 {
-    int result = random_digit - ((random_digit / 10) * 10);
-    Console.WriteLine(result);
-}
-else if (digit_len.Length == 4)
-{
-    int result = (random_digit - ((random_digit / 100) * 100)) / 10;
-    Console.WriteLine(result);
-}
-else if (digit_len.Length == 5)
-{
-    int result = (random_digit - ((random_digit / 1000) * 1000)) / 100;
-    Console.WriteLine(result);
+    if (digit_len.Length == 3)
+        {
+            int result = random_digit - ((random_digit / 10) * 10);
+        }
+    else if (digit_len.Length == 4)
+        {
+            int result = (random_digit - ((random_digit / 100) * 100)) / 10;
+        }
+    // else if (digit_len.Length == 5)
+    //     {
+    //         int result = (random_digit - ((random_digit / 1000) * 1000)) / 100;
+    //     }
+    Console.WriteLine("У числа " + random_digit + " третья цифра является " + result + ".\n\n");
 }
