891d7e35b9762b66d0be72994897216661ef611f
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-24 12:00:27 +0300

    task_1 logic

---
+++ hw.02_220822/task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿string task_name = "Напишите программу, которая принимает на вход случайно сгенерированное трёхзначное число и на выходе показывает вторую цифру этого числа.";
+int separator_length = 120;
+string separator = new String ('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+int random_digit = new Random().Next(100, 999);
+Console.WriteLine(random_digit);
+
+int digit_1 = random_digit / 100;
+// Console.WriteLine(digit_1);
+
+int digit_2 = random_digit - digit_1 * 100;
+// Console.WriteLine(digit_2);
+
+int digit_3 = digit_2 / 10;
+Console.WriteLine(digit_3);
+
+int result = (random_digit - (random_digit / 100) * 100) / 10;
+
+Console.WriteLine(result);
