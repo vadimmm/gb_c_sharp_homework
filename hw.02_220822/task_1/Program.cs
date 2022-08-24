e028d3558dc906ca85660821580c5e7045f7c8cc
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-24 12:07:15 +0300

    task_1 ok

--- hw.02_220822/task_1/Program.cs
+++ hw.02_220822/task_1/Program.cs
@@ -10,15 +10,6 @@ Console.WriteLine("");
 int random_digit = new Random().Next(100, 999);
 Console.WriteLine("Случайно сгенерированное трехзначное число: " + random_digit);
 
-int digit_1 = random_digit / 100;
-// Console.WriteLine(digit_1);
-
-int digit_2 = random_digit - digit_1 * 100;
-// Console.WriteLine(digit_2);
-
-int digit_3 = digit_2 / 10;
-// Console.WriteLine(digit_3);
-
 int result = (random_digit - (random_digit / 100) * 100) / 10;
 Console.Write("ОТВЕТ: ");
 Console.WriteLine("Для числа " + random_digit + " вторая цифра является " + result + ".\n\n");
