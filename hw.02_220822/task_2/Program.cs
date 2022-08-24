8d0e9ba9234c956a492e8cd5ec39fc01b4726743
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-24 14:20:33 +0300

    task_2 logic

---
+++ hw.02_220822/task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿string task_name = "Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа (от 1 до 65536) или сообщает, что третьей цифры нет.";
+int separator_length = 120;
+string separator = new String ('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+int random_digit = new Random().Next(1, 65536); //65536
+Console.WriteLine("Случайно сгенерированное число: " + random_digit);
+
+string digit_len = Convert.ToString(random_digit);
+Console.WriteLine(digit_len.Length);
+
+if (digit_len.Length <= 2)
+{
+    Console.WriteLine("У числа " + random_digit + " третьей цифры нет.");
+}
+else if (digit_len.Length == 3)
+{
+    int result = random_digit - ((random_digit / 10) * 10);
+    Console.WriteLine(result);
+}
+else if (digit_len.Length == 4)
+{
+    int result = (random_digit - ((random_digit / 100) * 100)) / 10;
+    Console.WriteLine(result);
+}
+else if (digit_len.Length == 5)
+{
+    int result = (random_digit - ((random_digit / 1000) * 1000)) / 100;
+    Console.WriteLine(result);
+}
