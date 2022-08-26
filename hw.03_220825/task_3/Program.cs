d38768a930c371505fd4550c3f135744bb18b0ce
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-26 20:54:04 +0300

    03.task_3 seminar ok

---
+++ hw.03_220825/task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿string task_name = @"Задача 3:
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.";
+int separator_length = 120;
+string separator = new String ('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+Console.Write("Пожалуйста, введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("ОТВЕТ: ");
+
+int[] array = new int [N];
+
+int index = 0;
+int num = 1;
+while (index < N)
+{
+    array[index] = (int) Math.Pow(num, 3);
+    Console.Write(array[index] + " ");
+    num++;
+    index++;
+}
