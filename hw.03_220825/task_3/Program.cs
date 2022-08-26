7a7f29c91dcc7a2e909f8dc1e8370af808f912d1
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-26 21:55:03 +0300

    03.task_3 student logic v1

--- hw.03_220825/task_3/Program.cs
+++ hw.03_220825/task_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿string task_name = @"Задача 3:
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.";
 int separator_length = 120;
-string separator = new String ('#', separator_length);
+string separator = new String('#', separator_length);
 Console.Clear();
 Console.WriteLine(separator);
 Console.WriteLine("\n" + task_name + "\n");
@@ -10,16 +10,15 @@ Console.WriteLine("");
 
 Console.Write("Пожалуйста, введите целое число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-Console.Write("ОТВЕТ: ");
+// int N = 3;
+// Console.Write("ОТВЕТ: ");
 
-int[] array = new int [N];
+int[] array = new int[N];
 
 int index = 0;
 int num = 1;
-while (index < N)
-{
-    array[index] = (int) Math.Pow(num, 3);
-    Console.Write(array[index] + " ");
-    num++;
-    index++;
-}
+
+for (Console.Write("ОТВЕТ: "); index < N; Console.Write(array[index] + " "), index++, num++)
+    array[index] = (int)Math.Pow(num, 3);
+// Console.Write(array[index] + " ");
+// num++;
