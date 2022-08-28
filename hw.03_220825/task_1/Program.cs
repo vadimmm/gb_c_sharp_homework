857cb7623e6215daefda1ec70cc369fbea38e4a6
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-28 22:12:42 +0300

    03.task_1 ok

--- hw.03_220825/task_1/Program.cs
+++ hw.03_220825/task_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿string task_name = @"Задача 1
 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Число хранить в типе данных int, решать с помощью арифметических операций (не рассматривая число как тип данных string).";
+Число хранить в типе данных int, решать с помощью арифметических операций
+(не рассматривая число как тип данных string).";
 int separator_length = 120;
 string separator = new String ('#', separator_length);
 Console.Clear();
@@ -14,18 +15,23 @@ int value = Convert.ToInt32(Console.ReadLine());
 int value_reverse = 0;
 int value_temp = value;
 
+// решение методом цикла while
 while (value_temp != 0)
 {
     int value_last = value_temp % 10;
     value_reverse = value_reverse * 10 + value_last;
     value_temp = value_temp / 10;
 }
-    
+
 if (value == value_reverse)
 {
-    Console.WriteLine("палиндром");
+    Console.BackgroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(" Число является палиндромом ");
+    Console.ResetColor();
 }
 else
 {
-    Console.WriteLine("не палиндром");
+    Console.BackgroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine(" Число не является палиндромом ");
+    Console.ResetColor();
 }
