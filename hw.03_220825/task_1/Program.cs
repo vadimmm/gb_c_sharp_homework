d2e31956d241c2e2b367393d007e1813b9644017
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-28 21:33:20 +0300

    03.task_1 logic v1 ok

---
+++ hw.03_220825/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿string task_name = @"Задача 1
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Число хранить в типе данных int, решать с помощью арифметических операций (не рассматривая число как тип данных string).";
+int separator_length = 120;
+string separator = new String ('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+Console.Write("Пожалуйста, введите целое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int value_reverse = 0;
+int value_temp = value;
+
+while (value_temp != 0)
+{
+    int value_last = value_temp % 10;
+    value_reverse = value_reverse * 10 + value_last;
+    value_temp = value_temp / 10;
+}
+    
+if (value == value_reverse)
+{
+    Console.WriteLine("палиндром");
+}
+else
+{
+    Console.WriteLine("не палиндром");
+}
