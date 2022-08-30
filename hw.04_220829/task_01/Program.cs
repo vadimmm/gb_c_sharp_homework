8e7ec3944af3da93ed4dcc7c223b1a3b4b162b38
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-30 16:32:53 +0300

    04.task_1 logic ok

---
+++ hw.04_220829/task_01/Program.cs
@@ -0,0 +1,27 @@
+﻿string task_name = @"Задача 1
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Не использовать функцию Math.Pow()";
+int separator_length = 120;
+string separator = new String('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+
+// Получаем на ввод данные
+Console.Write("Введите число 'A': ");
+int number_A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 'B': ");
+int number_B = Convert.ToInt32(Console.ReadLine());
+
+int number_A_tmp = number_A;
+
+for (int i = 1; i < number_B; i++)
+{
+    number_A = number_A_tmp * number_A;
+    // Console.WriteLine(number_A);    
+}
+
+Console.Write($"ОТВЕТ: {number_A}");
