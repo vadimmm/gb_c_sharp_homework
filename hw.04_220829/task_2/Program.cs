8f6e44f57412a1afbce9854cd92b99114351a16a
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-08-31 21:19:30 +0300

    04.taks_2 ok

--- hw.04_220829/task_2/Program.cs
+++ hw.04_220829/task_2/Program.cs
@@ -22,7 +22,6 @@ while (count_tmp > 0)
     count_tmp /= 10;
     countOfDigit++;
 }
-// Console.WriteLine("Длина числа: " + countOfDigit + " в числе " + number);
 
 // создать массив по длине числа
 int[] digits = new int[countOfDigit];
@@ -32,56 +31,41 @@ int tmp = number;
 for (int index = 0; index < countOfDigit; index++)
 {
     digits[index] = tmp % 10;
-    // Console.WriteLine($"Для индекса {index} записано число {digits[index]}");
     tmp /= 10;
 }
-
-// печать полученного массива
-// var str = string.Join(" ", digits);
-// Console.WriteLine("Данные в массиве: " + str);
 
 // проверка условия делителей
 int dividerSum = 0;
 int countDivider = 0;
 for (int index = 0; index < countOfDigit; index++)
 {
-    // Console.WriteLine($"Проверка {number} % {digits[index]}");
     if (digits[index] == 0)
     {
         Console.WriteLine($"Найдена цифра {digits[index]}, на неё делить нельзя. Проверяем следующее число из массива.");
     }
     else if (number % digits[index] == 0)
     {
-        // Console.WriteLine($"{number}{digits[index]}");
-        // Console.WriteLine($"Найдена подходящяя для условия цифра: {digits[index]}");
         dividerSum += digits[index];
         countDivider++;
     }
 }
 
-
 // вывод описания результата
-Console.WriteLine($"\nЧисло {number}\nКоличество делителей {countDivider}\nСумма делителей {dividerSum}\n\n");
+Console.Write($"\n\nОТВЕТ: ");
 
 if (countDivider == 0)
 {
-    Console.WriteLine($"Для числа {number} не найден делитель");    
+    Console.WriteLine($"Для числа {number} не найден делитель.\n\n");    
 }
 else if (countDivider == 1)
 {
-    Console.WriteLine($"Для числа {number}, найден один делитель, число {dividerSum}");
+    Console.WriteLine($"Для числа {number}, найден один делитель, число {dividerSum}.\n\n");
 }
 else if ((2 <= countDivider) && (countDivider <= 4))
 {
-    Console.WriteLine($"Для числа {number}, найдено {countDivider} делителя, сумма делителей составляет {dividerSum}");
+    Console.WriteLine($"Для числа {number}, найдено {countDivider} делителя, сумма делителей составляет {dividerSum}.\n\n");
 }
 else if ((5 <= countDivider) && (countDivider <= 20))
 {
-    Console.WriteLine($"Для числа {number}, найдено {countDivider} делителей, сумма делителей составляет {dividerSum}");
+    Console.WriteLine($"Для числа {number}, найдено {countDivider} делителей, сумма делителей составляет {dividerSum}.\n\n");
 }
-
-
-// 452 -> 6
-// 82 -> 2
-// 9012 -> 3
-// 23 -> 0
