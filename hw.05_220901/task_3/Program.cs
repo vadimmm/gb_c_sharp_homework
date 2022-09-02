53af006bc75428f6f59122a491207905de663b5e
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-09-02 11:30:12 +0300

    05.task_03 logic ok

--- hw.05_220901/task_3/Program.cs
+++ hw.05_220901/task_3/Program.cs
@@ -16,6 +16,9 @@ int N = Convert.ToInt32(Console.ReadLine());
 int numbStart = -2147483648;
 int numbStop = 2147483647;
 
+int numbMin = 0;
+int numbMax = 0;
+
 int[] numbers = new int[N];
 
 Random rnd = new Random();
@@ -26,39 +29,38 @@ for (int i = 0; i < N; i++)
 }
 
 
-void MinMaxArrayNumbers(string value)
-int numb = numbers[0];
+int MinMaxArrayNumbers(string value)
 {
+    int numb = numbers[0];
     if (value == "min")
     {
-        for(int i=0; i < N;i++)
+        for (int i = 0; i < N; i++)
         {
             if (numb < numbers[i])
             {
-               numb = numbers[i];
+                numb = numbers[i];
             }
         }
     }
     else if (value == "max")
     {
-        for(int i=0; i < N;i++)
+        for (int i = 0; i < N; i++)
         {
             if (numb > numbers[i])
             {
-               numb = numbers[i];
-            }   
+                numb = numbers[i];
+            }
         }
     }
     else
     {
         Console.Write("Не верные входные данные...");
-        return;
     }
     return numb;
 }
 
-int numbMin = MinMaxArrayNumbers("min");
-int numbMax = MinMaxArrayNumbers("max");
+numbMin = MinMaxArrayNumbers("min");
+numbMax = MinMaxArrayNumbers("max");
 
 int result = numbMax - numbMin;
 Console.WriteLine($"ОТВЕТ: Разница между максимальным {numbMax} и минимальным {numbMin} значение массива сосставляет {result}.");
