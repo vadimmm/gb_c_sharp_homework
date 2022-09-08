fb087e8bff3ba143ab25bbf1be0a53e93bc45455
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-09-08 10:09:41 +0300

    06.task_3 logic ok

---
+++ hw.06_220905/task_3/Program.cs
@@ -0,0 +1,44 @@
+﻿string task_name = @"Задача 3: * 
+Найдите сумму произведений пар чисел в одномерном целочисленном массиве. 
+Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+Если кол-во элементов нечетное, то элемент посередине не учитывать.
+Результат сложения вывести на экран.
+1, 3, 8, 3, 2 -> 11
+8, 3, 4, 2 -> 28";
+int separator_length = 120;
+string separator = new String ('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+Console.Write("Введите количество чисел для формирования случаныйх данных в массиве: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int numbStart = -2147483648;
+int numbStop = 2147483647;
+int sum = 0;
+
+int[] numbers = new int[N];
+
+Random rnd = new Random();
+
+for (int i = 0; i < N; i++)
+{
+    numbers[i] = rnd.Next(numbStart, numbStop);
+}
+
+var str = string.Join(" ", numbers);
+Console.WriteLine($"Сгенерированные числа в массиве: {str}\n\n");
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+    int numberFirst = numbers[i];
+    int numberSecond = numbers[numbers.Length - (1 +i)];
+    int numberMultiply = numberFirst * numberSecond;
+    sum += numberMultiply;
+    Console.WriteLine($"{numberFirst} * {numberSecond} = {numberMultiply}\n Накопленная сумма: {sum}\n ================");
+}
+
+Console.Write($"Сумма чисел: {sum}");
