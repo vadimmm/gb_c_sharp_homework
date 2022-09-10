835fb0e9b04076ef5eb2582aa475e69793f54b05
Vadim Safronov <109957474+gb2975vadim@users.noreply.github.com>
2022-09-10 12:05:40 +0300

    07.task_1 logic

---
+++ hw.07_220908/task_1/Program.cs
@@ -0,0 +1,49 @@
+﻿string task_name = @"Задача 1.
+Задайте двумерный массив размером MxN, заполненный случайными вещественными (тип double) числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9";
+int separator_length = 120;
+string separator = new String('#', separator_length);
+Console.Clear();
+Console.WriteLine(separator);
+Console.WriteLine("\n" + task_name + "\n");
+Console.WriteLine(separator);
+Console.WriteLine("");
+
+Console.Write("Введите число строк массива: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int strsCount = 3;
+// int columsCount = 4;
+
+int Mk = M;
+if (M > N) Mk = N - M;
+Console.WriteLine($"Случайынй интервал M={M}, N={N}. N больше Mk={Mk} на {N - Mk}");
+double[,] numbers = new double[M, N];
+
+
+// заполнение массива случайными числами
+Random rnd = new Random();
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        double k = rnd.Next(Mk, N);
+        numbers[i, j] = rnd.Next(-100, 100) / k;
+    }
+}
+
+// вывод содержимого массива
+Console.WriteLine("\n\nСодержимое массива после заполнения случайными данными:");
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        Console.Write($"{numbers[i, j]}\t");
+    }
+    Console.WriteLine("");
+}
